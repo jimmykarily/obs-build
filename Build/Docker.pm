79a0671ce00aa65aafb34888cbda32970e3e5667
Dimitris Karakasilis <jimmykarily@gmail.com>
2017-05-25 17:57:38 +0300

    Introduce obs_pkg_mgr script to install packages and add repos
    Also parse additional repositories and return them in the parser result

--- Build/Docker.pm
+++ Build/Docker.pm
@@ -45,12 +45,13 @@ sub parse {
   $dockerfile_data =~ s/[^\S\n\r]+$//gm;
 
   my @deps = ();
+  my @repos = ();
   my $pkg_string;
 
   # Match lines that start with "RUN" up to where the "RUN" command ends. It
   # might span multiple lines if "\" is used (handled with negative lookbehind).
   # E.g.
-  # RUN obs_install package1 package2 \
+  # RUN obs_pkg_mgr install package1 package2 \
   #   package3
   while ($dockerfile_data =~ /(^\s*RUN(?:.|\n|\r)*?(?<!\\)$)/gm) {
     my $run_command = $1;
@@ -58,14 +59,31 @@ sub parse {
     # Remove the Dockerfile escape character and merge multiple lines in one.
     $run_command =~ s/\\(\n|\r)*//gm;
 
-    # Match all obs_install commands up to the next command.
+    # Match all obs_pkg_mgr commands up to the next command.
     # A command stops at ";" or "&" or end of line. E.g.
-    #  RUN obs_install one \
-    #   two && ls ; obs_install three
-    while ($run_command =~ /obs_install\s+(.*?)(?:[;&]|$)/g) {
+    #  RUN obs_pkg_mgr install one \
+    #   two && ls ; obs_pkg_mgr install three
+    while ($run_command =~ /obs_pkg_mgr\s+install\s+(.*?)(?:[;&]|$)/g) {
       $pkg_string = $1;
       my @packages = split(/\s+/, $pkg_string);
       if (0+@packages != 0) { push @deps, @packages; }
+    }
+
+    # Supported command:
+    # RUN obs_pkg_mgr add_repo http://download.opensuse.org/repositories/Virtualization:/containers/openSUSE_Leap_42.2/ "Virtualization:Containers (openSUSE_Leap_42.2)"
+    # Adds this to @repos:
+    # "obs://Virtualization:containers/openSUSE_Leap_42.2/"
+    while ($run_command =~ /obs_pkg_mgr\s+add_repo\s+(.+?)\s+/g) {
+      my $repo_url = $1;
+      print "Converting repo to obs format: $repo_url\n";
+      # Convert string:/string2:/package to obs:/string:string2/package
+      if ($repo_url =~ /((?:[^\/]+:\/)*(?:[^\/]+?)\/([^\/]+\/?))$/) {
+        $repo_url = $1;
+        $repo_url =~ s/:\//:/;
+        push @repos, ("obs://$repo_url");
+      } else {
+        die "Format of additional repository not recognized";
+      }
     }
   }
 
@@ -75,9 +93,11 @@ sub parse {
   }
 
   print STDERR "Dependencies: @deps \n";
+  print STDERR "Repositories: @repos \n";
 
   my $ret = {};
   $ret->{'deps'} = \@deps;
+  $ret->{'path'} = \@repos;
 
   return $ret;
 }
